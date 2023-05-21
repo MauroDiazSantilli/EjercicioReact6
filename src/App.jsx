e9e3c4381ce5c25771f1c4df434dea5cc6b73fcf
MauroDS <mauro.diaz.santilli@gmail.com>
2023-05-21 12:18:49 -0300

    Dandole sombreado a formulario principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import {Container} from 'react-bootstrap'
 import './App.css'
-import Colores from "./Formulario"
+import Colores from "./components/Formulario"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
-    <Container className='my-5 mainPage'>
+    
     <Colores/>
-    </Container>
-    <footer className='bg-dark text-center text-secondary py-4'>
-      <p>&copy; Todos los derechos reservados</p>
-    </footer>
+    
     </>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,41 +1,49 @@
-import {Button,Form} from 'react-bootstrap';
+import { Button, Form, Container } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 
-function Colores(){
-const [color, setColor] = useState("")
+function Colores() {
+  const [color, setColor] = useState('');
 
-const handleChange = (e) => {
-    setColor(e.target.value)
-}
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
 
-const handleSubmit = (e) => {
-    e.preventDefault()
-    if(color.trim() !== ""){
-        localStorage.setItem("color", color)
-        setColor("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (color.trim() !== '') {
+      localStorage.setItem('color', color);
+      setColor('');
     }
-}
+  };
 
-useEffect(() => {
-    const colorStorage = localStorage.getItem("color")
-    if(colorStorage){
-        setColor(colorStorage)
+  useEffect(() => {
+    const colorStorage = localStorage.getItem('color');
+    if (colorStorage) {
+      setColor(colorStorage);
     }
-}, [])
+  }, []);
 
-return(
-    <div>
-    <h1>Administrar colores</h1>
-    <Form onSubmit={handleSubmit}>
-    <Form.Group>
-        <Form.Control type='text' value={color} placeholder = "Ingrese un color. Por ej: Verde" onChange={handleChange}/>     
-    </Form.Group>
-    <Button variant = "primary" type="submit">Guardar</Button>
-    </Form>
-    </div>
-  )
-
+  return (
+    <Container className='border my-5 p-5 shadow'>
+      <p>Administrar colores</p>
+      <Form onSubmit={handleSubmit}>
+        <Container className='bg-info p-4 d-flex align-items-center shadow-sm'>
+          <img src="" alt="Color elegido" className="me-2" />
+          <Form.Group className="mb-0">
+            <Form.Control
+              type="text"
+              value={color}
+              placeholder="Ingrese un color"
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Container>
+        <Button variant="primary" type="submit" className='mt-3 shadow-sm'>
+          Guardar
+        </Button>
+      </Form>
+    </Container>
+  );
 }
-
 
 export default Colores;
